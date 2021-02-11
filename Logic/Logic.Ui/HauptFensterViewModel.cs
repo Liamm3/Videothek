c3c85036533c2d49f89488a121ea2951e52ad307
Liam Wohlfart <liam.wohlfart0@gmail.com>
2021-02-11 17:05:54 +0100

    Projektdateien hinzufügen.

---
+++ Logic/Logic.Ui/HauptFensterViewModel.cs
@@ -0,0 +1,81 @@
+﻿using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Videothek.Logic.Ui.ViewModel
+{
+    public class HauptFensterViewModel : ViewModelBase
+    {
+        private SqlConnection conn = new SqlConnection();
+
+        private DataView selectedData;
+
+        public DataView SelectedData
+        {
+            get => selectedData;
+            set
+            {
+                selectedData = value;
+                RaisePropertyChanged("SelectedData");
+            }
+        }
+
+        private ICommand _OnTableSelect;
+
+        public ICommand OnTableSelect
+        {
+            get
+            {
+                if (_OnTableSelect == null)
+                {
+                    _OnTableSelect = new RelayCommand<string>((table) =>
+                    {
+                        SelectedData = GetDataViewOf(table);
+                    });
+                }
+
+                return _OnTableSelect;
+            }
+        }
+
+        public HauptFensterViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+            }
+            else
+            {
+                // Code runs "for real"
+            }
+        }
+
+        private DataView GetDataViewOf(string table)
+        {
+            DataTable dt = new DataTable();
+            conn.ConnectionString = "Data Source=W011076SYS\\SQLEXPRESS;" +
+                                    "Initial Catalog=Bibliothek;" +
+                                    "Integrated Security=SSPI;";
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            try
+            {
+                conn.Open();
+                adapter.SelectCommand = new SqlCommand("SELECT * FROM " + table, conn);
+                adapter.Fill(dt);
+                return dt.DefaultView;
+            }
+            catch (System.Exception e)
+            {
+                conn.Close();
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Logic/Logic.Ui/MainViewModel.cs
@@ -0,0 +1,19 @@
+using GalaSoft.MvvmLight;
+
+namespace Videothek.Logic.Ui.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+            }
+            else
+            {
+                // Code runs "for real"
+            }
+        }
+    }
+}
